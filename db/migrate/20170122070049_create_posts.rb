b3444b8d8f24323c082d20d24d09489b0f24bf5e
JuSugang <tnrkd7679@naver.com>
2017-01-22 14:51:15 +0000

    마이그레이션 파일과 모델 파일 작성하고 User, Post, Comment 테이블 만들기

--- db/migrate/20170122070049_create_posts.rb
+++ db/migrate/20170122070049_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+		t.integer :user_id
+		t.string :category
+		t.string :title
+		t.text :content
       t.timestamps
     end
   end
